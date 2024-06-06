ecdb75fa6d02b92444e2b5e8ed48ada74928ed5c
shams17786 <mshamsuddin448@gmail.com>
2024-06-06 12:20:27 +0500

    types error fixed

--- custom-types.d.ts
+++ custom-types.d.ts
@@ -1,5 +1,28 @@
 // custom-types.d.ts
 declare module 'cloudinary' {
-  const content: any;
-  export default content;
+  namespace cloudinary {
+    namespace v2 {
+      function config(options: {
+        cloud_name: string;
+        api_key: string;
+        api_secret: string;
+      }): void;
+
+      namespace uploader {
+        function upload(
+          file: string,
+          options: {
+            public_id?: string;
+            overwrite?: boolean;
+            invalidate?: boolean;
+            resource_type?: string;
+            chunk_size?: number;
+          },
+          callback: (error: any, result: any) => void
+        ): void;
+      }
+    }
+  }
+
+  export default cloudinary;
 }
